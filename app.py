db3fb75c2ae931df9df72aa450644c9194905eae
Jake Martens <martensj@carleton.edu>
2021-11-23 19:53:26 -0600

    More code cleanup

--- app.py
+++ app.py
@@ -4,7 +4,12 @@
     9 November 2021
 
     A Flask application that provides a website with an accompanying API to 
-    support that website. Provides union and strike information.
+    support that website. This code brings together each of the disparate
+    pieces of our code.
+
+    This website helps users find information about local labor unions,
+    ongoing strikes nationwide, and ongoing cases against violating
+    employers.
 '''
 import flask
 import argparse
@@ -14,26 +19,25 @@ app = flask.Flask(__name__, static_folder='static', template_folder='templates')
 app.register_blueprint(api.api, url_prefix='/api')
 
 
-# Is there anything else we need to do here?
 @app.route('/') 
 def home():
+    '''Renders the homepage for the site.'''
     return flask.render_template('index.html')
 
 @app.route('/search_unions', strict_slashes=False)
 def search_unions():
+    '''Renders the Search Unions page'''
     return flask.render_template('search_unions.html')
 
 @app.route('/search_cases') 
 def search_cases():
+    '''Renders the Search Cases page.'''
     return flask.render_template('search_cases.html')
 
 @app.route('/search_strikes') 
 def search_strikes():
+    '''Renders the Search Strikes page.'''
     return flask.render_template('search_strikes.html')
-
-@app.route('/state') 
-def state():
-    return flask.render_template('state.html')
 
 
 # Again, is the following appropriate, or is there a better way to do it?
--- static/webapp.js
+++ static/webapp.js
@@ -1,17 +1,21 @@
 /*
  * webapp.js
  * Chloe Morscheck and Jake Martens
+ * 
+ * The JavaScript file for our site. Main features include
+ * querying the underlying database and displaying results on 
+ * a given page.
+ * 
+ * Our code includes snippets adapted from Jeff Ondich as well
+ * as Shreya Nair and Elliott Hanson. The snippets are noted
+ * in the comments preceding each function.
  */
 
 window.onload = initialize;
 
 function initialize() {
-    let elementStrike = document.getElementById('submit_button_strikes');
-    let elementCases = document.getElementById('submit_button_cases');
-    
     if (document.title == 'Union Finder'){
         onIndexLoad();
-        initializeMap();
     } else if (document.title == 'Search Unions') {
         loadStates();
         let elementUnions = document.getElementById('submit_button_unions');
@@ -19,15 +23,17 @@ function initialize() {
     } else if (document.title == 'Search Strikes') {
         loadStates();
         loadStrikeIndustries()
+        let elementStrike = document.getElementById('submit_button_strikes');
         elementStrike.onclick = onSubmitButtonStrikesClicked;
     } else if (document.title == 'Search Cases') {
         loadRegions();
         loadStates();
+        let elementCases = document.getElementById('submit_button_cases');
         elementCases.onclick = onSubmitButtonCasesClicked;
     }
-    
 }
 
+// Load relevant industries for sorting on the strikes page
 function loadStrikeIndustries() {
     let url = getAPIBaseURL() + '/strike_industries'
     fetch(url, {method: 'get'})
@@ -48,6 +54,8 @@ function loadStrikeIndustries() {
         console.log(error);
     });
 }
+
+// Load states for sorting, used on all three search pages
 function loadStates() {
     let url = getAPIBaseURL() + '/states'
     fetch(url, {method: 'get'})
@@ -57,17 +65,21 @@ function loadStates() {
     .then(function(states) {
         let stateDropDown = document.getElementById('state_selector');
         let optionsBody = '<option value="">-</option>';
-        let page_diff = 'abbr';
+        
+        // This switch case is necessary due to differences in how API
+        // handles state name vs. abbreviations
         if (document.title == "Search Strikes") {
             page_diff = 'state';
         } else {
             page_diff = 'abbr';
         }
+
         for (let k = 0; k < states.length; k++) {
             let state = states[k];
             
             optionsBody += '<option value="' + state[page_diff] + '">' + state['state'] + '</option>';
         }
+
         stateDropDown.innerHTML = optionsBody;
 
         // Placing this piece here feels less than ideal, we cannot make the map
@@ -79,13 +91,12 @@ function loadStates() {
         }
     })
 
-    
-
     .catch(function(error) {
         console.log(error);
     });
 }
 
+// Load judicial regions for use on the Search Case page
 function loadRegions() {
     let url = getAPIBaseURL() + '/regions'
     fetch(url, {method: 'get'})
@@ -94,10 +105,8 @@ function loadRegions() {
 
     .then(function(regions) {
         let regionsDropDown = document.getElementById('region_selector')
-        // should be able to set options Body to '' once its actually working
         let optionsBody = '<option value="default">-</option>'
         for (let k = 0; k < regions.length; k++) {
-            let region = regions[k]
             optionsBody += '<option value="' + regions[k] + '">' + regions[k] + '</option>'
         }
         regionsDropDown.innerHTML = optionsBody
@@ -108,21 +117,20 @@ function loadRegions() {
     });
 }
 
-
+// For usage on Search Unions page. Handles query and displays
+// matching unions on the page.
 function onSubmitButtonUnionsClicked() {
     let stateSelector = document.getElementById('state_selector');
     let state = stateSelector.value;
     let citySelector = document.getElementById('by_city');
     let city = citySelector.value;
     let nameSelector = document.getElementById('by_name');
-    // nameselector vs by_name
-    let name = by_name.value;
+    let name = nameSelector.value;
     let membersSelector = document.getElementById('members_selector');
     let members = membersSelector.value;
+
     let url = getAPIBaseURL() + '/unions/?state_abbr=' 
                 + state + '&city=' + city + '&name_query=' + name + '&members=' + members;
-
-
     fetch(url, {method: 'get'})
 
     .then((response) => response.json())
@@ -149,18 +157,21 @@ function onSubmitButtonUnionsClicked() {
         console.log(error);
     });
 }
+
+// For usage on Search Cases page. Handles query and displays
+// matching cases on the page.
 function onSubmitButtonCasesClicked() {
     let stateSelector = document.getElementById('state_selector');
     let state = stateSelector.value;
     let nameSelector = document.getElementById('by_name');
-    let name = by_name.value;
+    let name = nameSelector.value;
     let caseNumberSelector = document.getElementById('by_case_number');
-    let case_number = by_case_number.value;
+    let caseNumber = caseNumberSelector.value;
     let regionSelector = document.getElementById('region_selector');
     let region = regionSelector.value;
-    let url = getAPIBaseURL() + '/cases/?state_abbr='
-                + state + '&name_query=' + name + '&case_number=' + case_number + '&region=' + region;
 
+    let url = getAPIBaseURL() + '/cases/?state_abbr='
+                + state + '&name_query=' + name + '&case_number=' + caseNumber + '&region=' + region;
     fetch(url, {method: 'get'})
 
     .then((response) => response.json())
@@ -175,13 +186,13 @@ function onSubmitButtonCasesClicked() {
             for (let k = 0; k < cases.length; k++) {
                 let result = cases[k];
                 if (result['date_closed'] == ''){
-                    var date_closed_string = 'Ongoing'
+                    var dateClosedString = 'Ongoing'
                 } else {
-                    var date_closed_string = result['date_closed']
+                    var dateClosedString = result['date_closed']
                 }
                 selectorBody += '<b>' + result['case_name'] + '</b> ('
                 + result['case_number'] + ')<br>Location: ' + result['city']+ ', ' + result['territory'] 
-                + '<br>Date Filed: ' + result['date_filed'] + '<br> Date Closed: ' + date_closed_string
+                + '<br>Date Filed: ' + result['date_filed'] + '<br> Date Closed: ' + dateClosedString
                 + '<br>Assigned Region: ' + result['region'] + '<br>Status: ' + result['current_status'] + '<br>';
             } 
         }
@@ -192,6 +203,9 @@ function onSubmitButtonCasesClicked() {
         console.log(error);
     });
 }
+
+// For usage on Search Strikes page. Handles query and displays
+// matching strikes on the page.
 function onSubmitButtonStrikesClicked() {
     let stateSelector = document.getElementById('state_selector');
     let state = stateSelector.value;
@@ -201,17 +215,15 @@ function onSubmitButtonStrikesClicked() {
     let endDate = endDateCheck.checked;
     let companySelector = document.getElementById('by_name')
     let company = companySelector.value;
-    //base url
+
     let url = getAPIBaseURL() + '/strikes/?state='
                 + state + '&industry=' + industry + '&company=' + company + '&end=' + endDate;
-    
     fetch(url, {method: 'get'})
 
     .then((response) => response.json())
     
     .then(function(strikes) {
         let resultsElement = document.getElementById('matching_strikes');
-        
         let selectorBody = '';
         if (strikes == "None") {
             selectorBody = "No strikes match search criteria."
@@ -219,15 +231,15 @@ function onSubmitButtonStrikesClicked() {
             for (let k = 0; k < strikes.length; k++) {
                 let strike = strikes[k];
                 if (strike['end_date'] == 'None'){
-                    var end_date_string  = 'present'
+                    var endDateString  = 'present'
                 } else {
-                    var end_date_string  = strike['end_date']
+                    var endDateString  = strike['end_date']
                 }
                 if (strike['participants'] == 'None'){
                     strike['participants'] = 'No data'
                 }
                 selectorBody += '<b>' + strike['employer'] + '</b><br>Number of Participants: ' + strike['participants']
-                + '<br>Started on ' + strike['start_date']+ ' to ' + end_date_string + '<br> Demands: ' + strike['demands'] 
+                + '<br>Started on ' + strike['start_date']+ ' to ' + endDateString + '<br> Demands: ' + strike['demands'] 
                 + '<br> Location: ' + strike['city'] + ', ' + strike['state'] + '<br>';
             }
         }
@@ -238,8 +250,12 @@ function onSubmitButtonStrikesClicked() {
         console.log(error);
     });
 }
+
+// Displays ongoing strikes as well as initializes map when
+// index.html is laoded.
 function onIndexLoad(){
-    // base url
+    initializeMap(); 
+
     let url = getAPIBaseURL() + '/strikes/';
     fetch(url, {method: 'get'})
 
@@ -255,12 +271,12 @@ function onIndexLoad(){
             for (let k = 0; k < 6; k++) {
                 let strike = strikes[k];
                 if (strike['end_date'] == 'None'){
-                    var end_date_string  = 'present'
+                    var endDateString  = 'present'
                 } else {
-                    var end_date_string  = strike['end_date']
+                    var endDateString  = strike['end_date']
                 }
                 selectorBody += '<b>' + strike['employer'] + '</b><br>' + 'Started on ' + strike['start_date']+ ' to ' 
-                + end_date_string + '<br> Demands: ' + strike['demands'] + '<br> Location: ' + strike['city'] + ', ' 
+                + endDateString + '<br> Demands: ' + strike['demands'] + '<br> Location: ' + strike['city'] + ', ' 
                 + strike['state'] + '<br>';
             }
         }
@@ -272,6 +288,9 @@ function onIndexLoad(){
     });
 }
 
+// Displays map on home page
+// Credit is due to Jeff Ondich, from whom most of
+// this code is adapted.
 function initializeMap() {
     var map = new Datamap({ element: document.getElementById('map-container'),
                             scope: 'usa', 
@@ -287,11 +306,13 @@ function initializeMap() {
                           });
 }
 
+// Allows interactivity with map display
 function onMapDone(dataMap) {
     dataMap.svg.selectAll('.datamaps-subunit').on('click', onStateClick);
 }
 
-
+// Displays name of state when user hovers over its
+// outline on the map.
 function hoverPopupTemplate(geography, data) {
     var template = '<div class="hoverpopup"><strong>' + geography.properties.name + '</strong><br>'
                     + '</div>';
@@ -299,10 +320,16 @@ function hoverPopupTemplate(geography, data) {
     return template;
 }
 
+// Navigates to Search Unions page when a state is clicked.
+// We include a hashtag with the state's abbreviation to 
+// trigger an automatic search for this state when the new 
+// page loads.
 function onStateClick(geography) {
     window.location.href = '/search_unions#' + geography.id;
 }
 
+// Called by loadStates() function. This function sets
+// the state selector to the abbreviation denoted in the URL
 function setState(){
     var hash = window.location.hash.substring(1);
     document.getElementById('state_selector').value = hash;
